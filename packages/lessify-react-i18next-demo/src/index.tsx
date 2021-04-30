138022b7bd6b947cc4061cf1adbefdc1b14067fb
alexcibotari <alexandru.cibotari@gmail.com>
2021-04-30 16:29:27 +0200

    move different repositories in one

--- packages/lessify-react-i18next-demo/src/index.tsx
+++ packages/lessify-react-i18next-demo/src/index.tsx
@@ -1,14 +1,44 @@
-import React from 'react';
+import React, {Suspense} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import i18next from "i18next";
+import I18NextHttpBackend from "i18next-http-backend";
+import {LessifyReactI18next} from "@lessify/react-i18next";
+import {initReactI18next} from 'react-i18next';
+
+i18next
+    .use(I18NextHttpBackend)
+    .use(initReactI18next)
+    .init(
+        {
+            fallbackLng: 'en',
+            lng: 'en',
+            debug: true,
+            keySeparator: false,
+            interpolation: {
+                escapeValue: false
+            },
+            backend: LessifyReactI18next(
+                {
+                    space: {
+                        spaceId: 'e600fed0-0674-11eb-8ebc-355c3e3200ae',
+                        environment: 'master',
+                        apiKey: 'api-key-Tqcgc-38872940-0d32-11eb-8a0e-61e5b1516e7a-Zlh7j'
+                    }
+                }
+            )
+        }
+    )
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Suspense fallback="loading">
+        <React.StrictMode>
+            <App/>
+        </React.StrictMode>
+    </Suspense>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ packages/lessify-react-i18next/README.md
@@ -0,0 +1,21 @@
+# lessify-react-i18next
+
+This library is library for integration of your React and `react-i18next` project with [Lessify](https://lessify.io/).
+
+# Integration
+
+## Testing
+In your project, link the `@lessify/react-i18next` we just built:
+
+``npm link $PATH_TO_PROJECT``
+
+Replace **$PATH_TO_PROJECT** with the path to the `@lessify/react-i18next` project’s root.
+Note that users will install instead of linking, this is just to iterate faster locally while developing.
+
+[comment]: <> (## NPM Publish)
+
+[comment]: <> (`npm init --scope=lessify`)
+
+[comment]: <> (`npm run build`)
+
+[comment]: <> (`npm publish --access public`)
